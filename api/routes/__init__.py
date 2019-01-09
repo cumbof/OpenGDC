78adf0799c74644383fa2828a16e93dd398ffc16
Fabio Cumbo <>
2019-01-09 15:45:35 +0100

    api removed

--- api/routes/__init__.py
+++
@@ -1,9 +0,0 @@
-#https://stackoverflow.com/a/36678789
-
-from flask import Blueprint
-blueprint = Blueprint('blueprint', __name__)
-
-from .basics import *
-from .data import *
-from .meta import *
-from .annotation import *
